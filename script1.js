ad9d15256caacdfa93ce0855f73c8e4e9053ab77
Deen-coder <alhassandeen005@gmail.com>
2024-12-15 15:58:45 +0000

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,20 +1,22 @@
-const conversionFactors = {
-    length: {
-        cm: { cm: 1, m: 0.01, in: 0.393701, ft: 0.0328084 },
-        m: { cm: 100, m: 1, in: 39.3701, ft: 3.28084 },
-        in: { cm: 2.54, m: 0.0254, in: 1, ft: 0.0833333 },
-        ft: { cm: 30.48, m: 0.3048, in: 12, ft: 1 }
+const lengthConversionFactors = {
+    cm: { cm: 1, m: 0.01, in: 0.393701, ft: 0.0328084 },
+    m: { cm: 100, m: 1, in: 39.3701, ft: 3.28084 },
+    in: { cm: 2.54, m: 0.0254, in: 1, ft: 0.0833333 },
+    ft: { cm: 30.48, m: 0.3048, in: 12, ft: 1 }
+};
+
+const temperatureConversionFunctions = {
+    celsius: {
+        fahrenheit: (value) => (value * 9/5) + 32,
+        kelvin: (value) => value + 273.15
     },
-    weight: {
-        kg: { kg: 1, g: 1000, lb: 2.20462, oz: 35.274 },
-        g: { kg: 0.001, g: 1, lb: 0.00220462, oz: 0.035274 },
-        lb: { kg: 0.453592, g: 453.592, lb: 1, oz: 16 },
-        oz: { kg: 0.0283495, g: 28.3495, lb: 0.0625, oz: 1 }
+    fahrenheit: {
+        celsius: (value) => (value - 32) * 5/9,
+        kelvin: (value) => (value - 32) * 5/9 + 273.15
     },
-    temperature: {
-        celsius: { celsius: (val) => val, fahrenheit: (val) => (val * 9 / 5) + 32, kelvin: (val) => val + 273.15 },
-        fahrenheit: { celsius: (val) => (val - 32) * 5 / 9, fahrenheit: (val) => val, kelvin: (val) => (val - 32) * 5 / 9 + 273.15 },
-        kelvin: { celsius: (val) => val - 273.15, fahrenheit: (val) => (val - 273.15) * 9 / 5 + 32, kelvin: (val) => val }
+    kelvin: {
+        celsius: (value) => value - 273.15,
+        fahrenheit: (value) => (value - 273.15) * 9/5 + 32
     }
 };
 
@@ -32,12 +34,17 @@ function convert() {
     let result;
 
     if (conversionType === "length") {
-        result = input * conversionFactors.length[fromUnit][toUnit];
-    } else if (conversionType === "weight") {
-        result = input * conversionFactors.weight[fromUnit][toUnit];
+        // Length conversion
+        result = input * lengthConversionFactors[fromUnit][toUnit];
+        document.getElementById("result").textContent = `${input} ${fromUnit} is equal to ${result} ${toUnit}`;
     } else if (conversionType === "temperature") {
-        result = conversionFactors.temperature[fromUnit][toUnit](input);
+        // Temperature conversion
+        if (fromUnit === toUnit) {
+            result = input; // No conversion needed if the units are the same
+        } else {
+            result = temperatureConversionFunctions[fromUnit][toUnit](input);
+            document.getElementById("result").textContent = `${input} ${fromUnit} is equal to ${result.toFixed(2)} ${toUnit}`;
+        }
     }
-
-    document.getElementById("result").textContent = `${input} ${fromUnit} is equal to ${result} ${toUnit}`;
 }
+
